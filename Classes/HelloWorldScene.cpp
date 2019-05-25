872b267860346323b88a6034a810cbe9a0059fc6
yuuyuu1655 <kana.yu1655@gmail.com>
2019-05-25 12:07:08 +0900

    スプライトの表示

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -101,20 +101,25 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    //// add "HelloWorld" splash screen"
+    //auto sprite = Sprite::create("HelloWorld.png");
+    //if (sprite == nullptr)
+    //{
+    //    problemLoading("'HelloWorld.png'");
+    //}
+    //else
+    //{
+    //    // position the sprite on the center of the screen
+    //    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
+    //    // add the sprite as a child to this layer
+    //    this->addChild(sprite, 0);
+    //}
+
+	Sprite*sprite = Sprite::create("DrStrangelove060Pyxurz.png");
+	this->addChild(sprite);
+
+
     return true;
 }
 
